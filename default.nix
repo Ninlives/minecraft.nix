b1952ef2da0a43728e3344f002f25cd490a84f10
mlatus <wqseleven@gmail.com>
2022-01-30 22:41:15 +0800

    saner build script

--- default.nix
+++ default.nix
@@ -2,7 +2,8 @@
 with lib;
 let
   inherit (pkgs)
-    fetchurl libpulseaudio libGL flite alsa-lib writeShellScript jq jre runCommand;
+    fetchurl libpulseaudio libGL flite alsa-lib unzip writeShellScriptBin jq jre
+    runCommand;
   inherit (pkgs.xorg) libXcursor libXrandr libXxf86vm;
   inherit (pkgs.writers) writePython3;
   manifests = importJSON ./vanilla/manifests.json;
@@ -21,7 +22,7 @@ let
   ];
 
   convertVersion = v: "v" + replaceStrings [ "." " " ] [ "_" "_" ] v;
-  libraryInfo = name:
+  fetchJar = name:
     let
       inherit (fabricLibraries.${name}) repo hash;
       splitted = splitString ":" name;
@@ -31,22 +32,24 @@ let
       path =
         "${replaceStrings [ "." ] [ "/" ] org}/${art}/${ver}/${art}-${ver}.jar";
       url = "${repo}/${path}";
-      jar = fetchurl {
-        inherit url;
-        ${hash.type} = hash.value;
-      };
-    in { inherit path jar; };
+    in fetchurl {
+      inherit url;
+      ${hash.type} = hash.value;
+    };
 
   auth = writePython3 "checkAuth" {
     libraries = with pkgs.python3Packages; [ requests pyjwt colorama ];
     flakeIgnore = [ "E501" "E402" "W391" ];
   } ''
-    ${builtins.replaceStrings ["@CLIENT_ID@"] [clientID] (builtins.readFile ./auth/msa.py)}
+    ${builtins.replaceStrings [ "@CLIENT_ID@" ] [ clientID ]
+    (builtins.readFile ./auth/msa.py)}
     ${builtins.readFile ./auth/login.py}
   '';
-  checkAuthOpts = let
-    json = "${jq}/bin/jq --raw-output";
-    text = ''
+
+  launchScript = { mainClass, versionInfo, javaLibraries, nativeLibraries
+    , assets, mods ? [ ] }:
+    let json = "${jq}/bin/jq --raw-output";
+    in writeShellScriptBin "minecraft" ''
       RED='\033[0;31m'
       FIN='\033[0m'
       PROFILE="$HOME/.local/share/minecraft.nix/profile.json"
@@ -61,30 +64,60 @@ let
       UUID=$(${json} '.["id"]' "$PROFILE")
       USER_NAME=$(${json} '.["name"]' "$PROFILE")
       ACCESS_TOKEN=$(${json} '.["mc_token"]["__value"]' "$PROFILE")
-    '';
-  in concatMapStringsSep " \\\n"
-  (t: "--run '${replaceStrings [ "'" ] [ "'\\''" ] t}'")
-  (splitString "\n" text);
 
-  generateWrapper = { mainClass, version, assetsIndex, mods ? [ ] }: ''
-    makeWrapper ${jre}/bin/java $out/bin/minecraft \
-        ${checkAuthOpts} \
-        --add-flags "-Djava.library.path='$out/natives'" \
-        --add-flags "-cp '$(find $out/libraries -name '*.jar' | tr -s '\n' ':')'" \
+      export LD_LIBRARY_PATH=${libPath}''${LD_LIBRARY_PATH:+':'}$LD_LIBRARY_PATH
+      exec ${jre}/bin/java \
+        -Djava.library.path='${
+          concatMapStringsSep ":" (native: "${native}/lib") nativeLibraries
+        }' \
+        -cp '${concatStringsSep ":" javaLibraries}' \
         ${
-          optionalString (mods != [ ]) ''
-            --add-flags "-Dfabric.addMods='$(find $out/mods -name '*.jar' | tr -s '\n' ':')'"''
+          optionalString (mods != [ ])
+          "-Dfabric.addMods='${concatStringsSep ":" mods}'"
         } \
-        --add-flags "${mainClass}" \
-        --add-flags "--version ${version}" \
-        --add-flags "--assetsDir $out/assets" \
-        --add-flags "--assetIndex ${assetsIndex}" \
-        --add-flags '--uuid "$UUID"' \
-        --add-flags '--accessToken "$ACCESS_TOKEN"' \
-        --prefix LD_LIBRARY_PATH : "${libPath}"
-  '';
+        ${mainClass} \
+        --version "${versionInfo.id}" \
+        --assetsDir "${assets}" \
+        --assetIndex "${versionInfo.assets}" \
+        --uuid "$UUID" \
+        --accessToken "$ACCESS_TOKEN" \
+        "$@"
+    '';
 
-  buildVanillaClient = versionInfo: assetsIndex:
+  buildVanillaLibraries = artifacts:
+    map (lib: fetchurl { inherit (lib.downloads.artifact) url sha1; })
+    (filter (x: !(x.downloads ? "classifiers")) artifacts);
+  buildNativeLibraries = artifacts:
+    map (artif:
+      let
+        zip = fetchurl {
+          inherit (artif.downloads.classifiers.${artif.natives.${OS}}) url sha1;
+        };
+      in runCommand "native" { buildInputs = [ unzip ]; } ''
+        mkdir -p $out/lib
+        unzip ${zip} -d $out/lib
+        rm -rf $out/lib/META-INF
+      '') (filter (x: (x.downloads ? "classifiers")) artifacts);
+  buildAssets = versionInfo: assetsIndex:
+    runCommand "assets" { } ''
+      ${concatStringsSep "\n" (builtins.attrValues
+        (flip mapAttrs assetsIndex.objects (name: a:
+          let
+            asset = fetchurl {
+              sha1 = a.hash;
+              url = "http://resources.download.minecraft.net/" + hashTwo;
+            };
+            hashTwo = builtins.substring 0 2 a.hash + "/" + a.hash;
+          in ''
+            mkdir -p $out/objects/${builtins.substring 0 2 a.hash}
+            ln -sf ${asset} $out/objects/${hashTwo}
+          '')))}
+      mkdir -p $out/indexes
+      ln -s ${builtins.toFile "assets.json" (builtins.toJSON assetsIndex)} \
+          $out/indexes/${versionInfo.assets}.json
+    '';
+  buildFabricLibraries = libraries: map (lib: fetchJar lib) libraries;
+  buildArguments = versionInfo: assetsIndex:
     let
       client = fetchurl { inherit (versionInfo.downloads.client) url sha1; };
       isAllowed = artifact:
@@ -101,97 +134,33 @@ let
         else
           true;
       artifacts = lib.filter isAllowed versionInfo.libraries;
+    in {
+      javaLibraries = buildVanillaLibraries artifacts ++ [ client ];
+      nativeLibraries = buildNativeLibraries artifacts;
+      assets = buildAssets versionInfo assetsIndex;
+    };
 
-    in pkgs.runCommand "minecraft-client-${versionInfo.id}" {
-      buildInputs = [ pkgs.unzip pkgs.makeWrapper ];
-      passthru.meta.mainProgram = "minecraft";
-    } ''
-      mkdir -p $out/bin $out/assets/indexes $out/libraries $out/natives
-      ln -s ${client} $out/libraries/client.jar
-
-      # Java libraries
-      ${concatMapStringsSep "\n" (artif:
-        let library = fetchurl { inherit (artif.downloads.artifact) url sha1; };
-        in ''
-          mkdir -p $out/libraries/${
-            builtins.dirOf artif.downloads.artifact.path
-          }
-          ln -s ${library} $out/libraries/${artif.downloads.artifact.path}
-        '') (filter (x: !(x.downloads ? "classifiers")) artifacts)}
-
-      # Native libraries
-      ${concatMapStringsSep "\n" (artif:
-        let
-          library = fetchurl {
-            inherit (artif.downloads.classifiers.${artif.natives.${OS}})
-              url sha1;
-          };
-        in ''
-          unzip ${library} -d $out/natives && rm -rf $out/natives/META-INF
-        '') (filter (x: (x.downloads ? "classifiers")) artifacts)}
-
-      # Assets
-      ${concatStringsSep "\n" (builtins.attrValues
-        (flip mapAttrs assetsIndex.objects (name: a:
-          let
-            asset = fetchurl {
-              sha1 = a.hash;
-              url = "http://resources.download.minecraft.net/" + hashTwo;
-            };
-            hashTwo = builtins.substring 0 2 a.hash + "/" + a.hash;
-          in ''
-            mkdir -p $out/assets/objects/${builtins.substring 0 2 a.hash}
-            ln -sf ${asset} $out/assets/objects/${hashTwo}
-          '')))}
-      ln -s ${builtins.toFile "assets.json" (builtins.toJSON assetsIndex)} \
-          $out/assets/indexes/${versionInfo.assets}.json
-
-      # Launcher
-      ${generateWrapper {
-        inherit (versionInfo) mainClass;
-        version = versionInfo.id;
-        assetsIndex = versionInfo.assets;
-      }}
-    '';
+  buildVanillaClient = versionInfo: assetsIndex:
+    launchScript (buildArguments versionInfo assetsIndex // {
+      inherit versionInfo;
+      inherit (versionInfo) mainClass;
+    });
 
   buildFabricClient = versionInfo: assetsIndex: fabricProfile: mods:
     let
       vanilla = buildVanillaClient versionInfo assetsIndex;
       loaderVersion = fabricProfile.loader;
       loader = fabricLoaders.${loaderVersion};
-      libraries = fabricProfile.libraries.client ++ loader.libraries;
+      extraJavaLibraries = buildFabricLibraries
+        (fabricProfile.libraries.client ++ loader.libraries);
       mainClass = loader.mainClass.client;
-    in runCommand "${vanilla.name}-fabric" {
-      buildInputs = [ pkgs.unzip pkgs.makeWrapper pkgs.xorg.lndir ];
-      passthru.withMods = extraMods:
-        buildFabricClient versionInfo assetsIndex fabricProfile
-        (mods ++ extraMods);
-      passthru.meta.mainProgram = "minecraft";
-    } ''
-      mkdir -p $out
-      lndir -silent ${vanilla} $out
-
-      # Java libraries
-      ${concatMapStringsSep "\n" (lib:
-        let inherit (libraryInfo lib) path jar;
-        in ''
-          mkdir -p $out/libraries/${builtins.dirOf path}
-          ln -s ${jar} $out/libraries/${path}
-        '') libraries}
-
-      ${optionalString (mods != [ ]) ''
-        mkdir -p $out/mods
-        ${concatMapStringsSep "\n" (mod: "ln -s ${mod} $out/mods") mods}
-      ''}
-
-      # Launcher
-      rm $out/bin/minecraft
-      ${generateWrapper {
-        inherit mainClass mods;
-        version = versionInfo.id;
-        assetsIndex = versionInfo.assets;
-      }}
-    '';
+      arguments = buildArguments versionInfo assetsIndex;
+    in launchScript {
+      inherit mainClass;
+      inherit versionInfo;
+      inherit (arguments) assets nativeLibraries;
+      javaLibraries = arguments.javaLibraries ++ extraJavaLibraries;
+    };
 
   buildMc = versionInfo: assetsIndex: fabricProfile:
     let
