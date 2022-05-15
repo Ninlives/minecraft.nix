d80697095486f05cf05eddf1880e8ba9d8a2fced
mlatus <wqseleven@gmail.com>
2022-05-15 14:53:02 +0800

    setup auto update

--- flake.nix
+++ flake.nix
@@ -17,11 +17,24 @@
             "osx"
           else
             builtins.throw "Unsupported system ${system}";
+        py = pkgs.python3.withPackages (p: [ p.requests ]);
       in {
         legacyPackages = lib.makeOverridable (import ./builder.nix) {
           inherit pkgs lib OS;
           # Users may override this with their own application id
           clientID = "adf6c624-b9ba-472e-9469-e54cc8f98e87";
+        };
+        apps.update = mkApp {
+          drv = let
+            snippet = dir: ''
+              pushd ./${dir}
+              ${py}/bin/python update.py
+              popd
+            '';
+          in pkgs.writeShellScriptBin "update" ''
+            ${snippet "fabric"}
+            ${snippet "vanilla"}
+          '';
         };
       });
 }
